05a55ddc51ceaac8db7235d260ef278a1ce9e346
rudyxwhite <brandonrwing98@gmail.com>
2023-03-22 09:03:05 -0500

    fixed the foreign keys so I would stop getting sql errors

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,7 +7,6 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey:'category_id',
-  onDelete: 'CASCADE'
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -17,14 +16,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'product_tag',
-  foreignKey: 'tag_id',
-  onDelete: 'CASCADE'
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'product_tag',
   foreignKey: 'tag_id',
-  onDelete: 'CASCADE'
 });
 module.exports = {
   Product,
