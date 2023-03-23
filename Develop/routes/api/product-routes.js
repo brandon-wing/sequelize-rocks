b2f7d5a709793aa5d9b41ec774459536e4274cbf
rudyxwhite <brandonrwing98@gmail.com>
2023-03-22 21:44:30 -0500

    product delete request working

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -127,10 +127,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 
   console.log("Params: ", req.params);
   // delete one product by its `id` value
+  try {
+    const productToDelete = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.status(200).json(productToDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
