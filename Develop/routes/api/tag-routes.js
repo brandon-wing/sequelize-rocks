626e630779c1e71259603b89a1622c7d5e2b90be
rudyxwhite <brandonrwing98@gmail.com>
2023-03-22 22:49:40 -0500

    fixed a tag route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try{
-    const oneTag = await Category.findByPk(req.params.id, {
+    const oneTag = await Tag.findByPk(req.params.id, {
       include: {
         model: Product
       }
