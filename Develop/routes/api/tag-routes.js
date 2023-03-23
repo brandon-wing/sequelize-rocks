a81538a299009bc88f3759c47514171425477103
rudyxwhite <brandonrwing98@gmail.com>
2023-03-22 22:32:25 -0500

    should be up and running

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -45,12 +45,33 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    let updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.json(updatedTag);
+  }
+  catch (err){
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagToDelete = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.status(200).json(tagToDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
